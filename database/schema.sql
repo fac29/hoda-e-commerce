b13595db9089ba83c909e7fefae12a97dc8aaef8
diddydeeable <diddydeeable@gmail.com>
2024-06-11 13:38:54 +0100

    added an author

--- database/schema.sql
+++ database/schema.sql
@@ -3,6 +3,7 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS products (
   product_id INTEGER PRIMARY KEY AUTOINCREMENT,
   product_name TEXT,
+  product_author TEXT,
   product_description TEXT,
   category TEXT,
   price INTEGER,
