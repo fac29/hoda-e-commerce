3a325bb493cc6f38487f873cbe068985feed2ff5
Alex Perez-Davies <alexperezdavies@gmail.com>
2024-06-12 10:48:02 +0100

    Update product.ts

--- model/product.ts
+++ model/product.ts
@@ -62,9 +62,7 @@ SELECT
 WHERE products.product_name = ?
 `);
 export function getProductByName(name: string) {
-    return select_product_by_name('To Kill a Mockingbird');
+    return select_product_by_name(name);
 }
 
-
-console.log(getProductByName)
 // export function getProductByDescription() {}
