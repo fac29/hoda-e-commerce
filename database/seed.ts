dc60a7f6db8841371dc683c20bfad2c557978a95
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-06-12 16:05:36 +0100

    changed to db.ts

--- database/seed.ts
+++ database/seed.ts
@@ -1,8 +1,7 @@
 const { readFileSync } = require('node:fs');
 const { join } = require('node:path');
-const Database = require('better-sqlite3');
 require('dotenv').config();
-const db = require('./db.js');
+const db = require('./db.ts');
 
 const seedPath = join('database', 'seed.sql');
 const seed = readFileSync(seedPath, 'utf-8');
