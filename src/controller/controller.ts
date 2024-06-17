45adff59b3cdfb24cb1f5b4a594e4ee3d8486ab4
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-06-17 16:39:43 +0100

    copy Dianas changes

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,4 +1,24 @@
-export async function getProductID(req: Request, res: Response) {
+import { Request, Response } from 'express';
+import {
+    getProductByID,
+    getProductBySearchTerm,
+    listProductsAll,
+} from '../../model/product';
+
+export function getAllProducts(req: Request, res: Response) {
+    try {
+        const searchQuery = req.query.search;
+        if (searchQuery) {
+            const stringQ = searchQuery.toString();
+            res.status(200).send(getProductBySearchTerm(stringQ));
+        }
+        let result = listProductsAll();
+        res.status(200).send(result);
+    } catch (error: any) {
+        res.status(500).send(error.toString);
+    }
+}
+export function getProductID(req: Request, res: Response) {
     try {
         const product = req.params.id;
         const productId = parseInt(product);
@@ -6,5 +26,21 @@ export async function getProductID(req: Request, res: Response) {
         res.status(200).send(result);
     } catch (error: any) {
         res.status(500).send(error.toString());
+    }
+}
+
+export function checkout(req: Request, res: Response) {
+    try {
+        //do something
+    } catch {
+        //do something else
+    }
+}
+
+export function login(req: Request, res: Response) {
+    try {
+        //do something
+    } catch {
+        //do something else
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import productRouter from './routes/productRouter';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('E-Commerce App: Express + TypeScript Server');
-});
-
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
-const { createProduct } = require('../model/product');
+app.use('/products', productRouter);
