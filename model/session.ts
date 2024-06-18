aacdb269269fe39110c374c89ca4a720989a4ac8
Alex <alexperezdavies@gmail.com>
2024-06-18 16:43:44 +0100

    added Session type

--- model/session.ts
+++ model/session.ts
@@ -1,6 +1,11 @@
 import { db } from './product';
 const crypto = require('node:crypto');
 
+type Session = {
+    user_id: number;
+    sid: string;
+};
+
 const insert_session = db.prepare(/*sql*/ `
     INSERT INTO sessions (session_id, user_id, expires_at)
     VALUES (
@@ -9,7 +14,7 @@ const insert_session = db.prepare(/*sql*/ `
       DATE('now', '+7 day'))
     `);
 
-export function createSession(user_id: number) {
+export function createSession(user_id: Session['user_id']) {
     const session_id = crypto.randomBytes(18).toString('base64');
     insert_session.run({ session_id, user_id });
     return session_id;
@@ -20,7 +25,7 @@ const select_session = db.prepare(`
     FROM sessions WHERE session_id = ?
   `);
 
-export function getSession(sid: string) {
+export function getSession(sid: Session['sid']) {
     const session = select_session.get(sid); // Retrieve session from the database
     if (session) {
         return session; // Ensure a plain object is returned
