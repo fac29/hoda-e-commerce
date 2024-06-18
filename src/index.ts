99df6bf51b79fb2af620bad644732d454a9def05
diddydeeable <diddydeeable@gmail.com>
2024-06-18 12:42:54 +0100

    origin for cors added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import productRouter from './routes/productRouter';
+const CORS = require('cors');
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+var corsOptions = {
+    origin: ['http://localhost:5173',/^localhost:'/],
+    optionsSuccessStatus: 200,
+   // origin:true -> if you want to allow requests from all origins
+};
+
+app.use(CORS(corsOptions));
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
