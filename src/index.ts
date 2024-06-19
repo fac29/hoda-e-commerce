67f6caf5978b93ac7459d9392833b2b3461519af
Alex <alexperezdavies@gmail.com>
2024-06-19 11:32:47 +0100

    use cookie parser and signup route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+const cookieParser = require('cookie-parser');
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import productRouter from './routes/productRouter';
+import signUpRouter from './routes/signupRouter';
 
 const CORS = require('cors');
 
@@ -8,6 +10,7 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const cookies = cookieParser(process.env.COOKIE_SECRET);
 
 var corsOptions = {
     origin: ['http://localhost:5173', /^localhost:'/],
@@ -15,10 +18,13 @@ var corsOptions = {
     // origin:true -> if you want to allow requests from all origins
 };
 
+app.use(cookies);
 app.use(CORS(corsOptions));
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
 app.use('/products', productRouter);
+app.use(signUpRouter);
